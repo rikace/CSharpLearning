9aa8aa832d5135e75225978903bd3a64329e8384
sravani <sravani.kolluru1992@gmail.com>
2022-10-26 22:13:48 +0530

    guessing numer prog

--- src/CSharpLearning/Module1/Workshop1/Program.cs
+++ src/CSharpLearning/Module1/Workshop1/Program.cs
@@ -13,7 +13,36 @@ namespace Workshop1
         // See Docs folder Activity 1
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+
+            int randomNum = random.Next(1,10);
+            int chances = 4;
+            int userNum;
+            
+            Console.WriteLine("Guess the Number between 1 to 9");
+            string temp = Console.ReadLine();
+            userNum = int.Parse(temp);
+
+            while (randomNum != userNum && chances >= 1)
+            {
+                Console.WriteLine("{0} chance left and try again", chances);
+
+                temp = Console.ReadLine();
+                userNum = int.Parse(temp);
+
+                chances--;
+
+            }
+			
+            if(randomNum == userNum)
+            {
+                Console.WriteLine("Great! you got right number");
+            }
+            else
+            {
+                Console.WriteLine("exceeded maximum limit");
+            }
+            
         }
     }
 }
