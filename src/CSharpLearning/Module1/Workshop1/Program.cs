8abf65f70730db60528154907e993970a9e3d3f1
FAREAST\v-vipulb <v-vipulb@microsoft.com>
2022-10-26 14:41:17 -0700

    Worked on Workshop1 in Module1

--- src/CSharpLearning/Module1/Workshop1/Program.cs
+++ src/CSharpLearning/Module1/Workshop1/Program.cs
@@ -13,7 +13,40 @@ namespace Workshop1
         // See Docs folder Activity 1
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int RandomNumber = new Random().Next(0,10);
+            int Chances = 5;
+
+            for (int i = 0; i < Chances; i++)
+            {
+                Console.Write("Please enter the number in the range of 0-9: ");
+                try
+                {
+                    int EnteredNumber = Convert.ToInt32(Console.ReadLine());
+                    if (EnteredNumber == RandomNumber)
+                    {
+                        Console.WriteLine("Consgratulations. Number matched");
+                        break;
+                    }
+                    else
+                    {
+                        if (i == Chances - 1)
+                        {
+                            Console.WriteLine("Sorry, No more chances left.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Number did not matched. " + (Chances - i - 1) + " Chances left");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                catch(Exception)
+                {
+                    Console.Write("Please enter correct number in the range of 0-9. ");
+                    Console.WriteLine((Chances - i - 1) + " Chances left");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
