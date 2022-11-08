390a3d3f728c8e5e01bf80c649a05c98394c0481
Abhivk1102 <116644083+Abhivk1102@users.noreply.github.com>
2022-11-08 14:28:57 +0530

    <Workshop1 FizzBuzz>

---
+++ Workshop1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WorkShop1
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            FissBuzz.Run();
+
+            Console.ReadLine();
+        }
+
+    }
+}
