4cc1d18db99a2c234a861b038b169a11547ff1b8
Abhivk1102 <116644083+Abhivk1102@users.noreply.github.com>
2022-11-08 14:49:40 +0530

    WorkShop3 MergingCircle

---
+++ Workshop2/Program.cs
@@ -0,0 +1,10 @@
+public static class Solution
+{
+    public static void Main()
+    {
+        var circle1 = new Circle(3);
+        var circle2 = new Circle(3);
+        var circle3 = circle1 + circle2;
+        Console.WriteLine($"Adding circles of radius of {circle1.Radius} and {circle2.Radius} " + $"results in a new circle with a radius{circle3.Radius}");
+    }
+}
